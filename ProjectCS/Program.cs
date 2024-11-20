f6374cf4d351e4d05d983a53821cd65418a00eaf
Manhnguyenx <137817499+Manhnguyenx@users.noreply.github.com>
2024-11-20 22:39:03 +0700

    Test Project

---
+++ ProjectCS/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Your Name : ");
+
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Hello , " + name);
+    }
+}
